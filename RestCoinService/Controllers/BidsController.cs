434f57fe3ab9ee3b5a7d344a971a941c36948bfe
Izabela Kuzniar <iza8848@gmail.com>
2018-10-29 12:01:38 +0100

    first commit

---
+++ RestCoinService/Controllers/BidsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestCoinService.Model;
+
+namespace RestCoinService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BidsController : ControllerBase
+    {
+        static readonly HttpClient Client = new HttpClient();
+        private static string _uri = "http://localhost:58162/api/bids";
+
+        private static List<Bid> listOfBids = new List<Bid>()
+        {
+            new Bid(1, "Gold DK 1640", 2500, "Mike"),
+            new Bid(2, "Gold NL 1764", 5000, "Anbo"),
+            new Bid(3, "Gold FR1644", 35000, "Hammer"),
+            new Bid(4, "Silver GR 333", 2500, "Mike")
+        };
+
+        // GET: api/Bids
+        [HttpGet]
+        public IEnumerable<Bid> Get()
+        {
+            return listOfBids;
+        }
+
+        // GET: api/Bids/5
+        [HttpGet("{id}", Name = "Get")]
+        public Bid Get(int id)
+        {
+            Bid obj = listOfBids.Find(b => b.Id == id);
+            return obj;
+        }
+
+        // POST: api/Bids
+        [HttpPost]
+        public void Post([FromBody] Bid newBid)
+        {
+            listOfBids.Add(newBid);
+        }
+
+        // PUT: api/Bids/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Bid obj = listOfBids.Find(b => b.Id == id);
+            listOfBids.Remove(obj);
+        }
+    }
+}
---
+++ RestCoinService/Model/Bid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestCoinService.Model
+{
+    public class Bid
+    {
+        public Bid(int id, string item, double price, string name)
+        {
+            Id = id;
+            Item = item;
+            Price = price;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public double Price { get; set; }
+        public string Name { get; set; }
+
+        public Bid()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Id + " " + Item + " " + Price + " " + Name;
+        }
+    }
+}
